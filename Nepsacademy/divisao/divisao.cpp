a7a14947b945d43ff7b5179a55d93aa4cf8d2d5e
Isabela Medeiros <135226718+belamedeirosbl@users.noreply.github.com>
2024-05-14 17:49:53 -0300

    Create divisao.cpp

---
+++ Nepsacademy/divisao/divisao.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    double a,b;
+    cin >> a >> b;
+    double divisao = a/b;
+    printf("%.2f\n",divisao);
+    return 0;
+}
