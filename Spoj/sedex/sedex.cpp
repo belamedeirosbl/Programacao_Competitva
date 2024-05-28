7c2e245cb4e9b186bb184e895fefb8bb7d1ad67c
Isabela Medeiros <135226718+belamedeirosbl@users.noreply.github.com>
2024-05-28 15:54:41 -0300

    questão resolvida!

---
+++ Spoj/sedex/sedex.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int buscabinaria(int list[], int num, int size){
+    int inicio = 0, fim = size - 1;
+    while(inicio <= fim){
+        int meio = ((inicio + fim)/2);
+        if(list[meio] == num){
+            while(list[meio - 1] == num){
+                meio = meio - 1;
+            }
+            return meio;
+        }
+        if(list[meio] < num){
+            inicio = meio+1;
+        }else{
+            fim = meio-1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int size, testes;
+    scanf("%d %d", &size, &testes);
+    int list[size];
+    for(int i = 0; i < size; i++){
+        scanf("%d", &list[i]);
+    }
+    for(int cases; cases < testes; cases++){
+        int num;
+        scanf("%d", &num);
+        int resp = buscabinaria(list, num, size);
+        printf("%d\n", resp);
+    }
+
+    return 0;
+}
